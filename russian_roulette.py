f946544409abf50a57cb116c530bc4920f451dbc
Daniil Oreshnikov <doreshhh@yandex-team.ru>
2025-04-18 18:16:04 +0300

    Code refactoring

--- russian_roulette.py
+++ russian_roulette.py
@@ -8,27 +8,18 @@ import random
 import os
 import sys
 
+# WORKS ONLY ON WINDOWS
 def bsod():
     """Generate a BSOD."""
     from ctypes import POINTER, byref, c_int, c_uint, c_ulong, windll
 
     nullptr = POINTER(c_int)()
 
-    windll.ntdll.RtlAdjustPrivilege(
-        c_uint(19),
-        c_uint(1),
-        c_uint(0),
-        byref(c_int())
-    )
+    # made this a oneliner
+    windll.ntdll.RtlAdjustPrivilege(c_uint(19), c_uint(1), c_uint(0), byref(c_int()))
 
-    windll.ntdll.NtRaiseHardError(
-        c_ulong(0xC000007B),
-        c_ulong(0),
-        nullptr,
-        nullptr,
-        c_uint(6),
-        byref(c_uint())
-    )
+    # and this too
+    windll.ntdll.NtRaiseHardError(c_ulong(0xC000007B), c_ulong(0), nullptr, nullptr, c_uint(6), byref(c_uint()))
 
 chambers = input("Please enter the number of chambers (default = 6): ")
 
